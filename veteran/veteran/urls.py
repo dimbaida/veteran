4bce82bc044e114be8b661df58a8467121f83197
dim-mac <dimbaida@gmail.com>
2024-06-28 10:50:17 +0300

    month and year for Measurement

--- veteran/veteran/urls.py
+++ veteran/veteran/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authuser/', include('authuser.urls')),
-    path('electricity/', include('electricity.urls'))
+    path('electricity/', include('electricity.urls')),
+    path('', lambda request: redirect('electricity/measurements/', permanent=True)),
 ]
 
