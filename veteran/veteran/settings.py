51769aa5981b19d4b251b73568b625a2d3f652f8
dim-mac <dimbaida@gmail.com>
2024-07-07 20:20:33 +0300

    ukrainian localization

--- veteran/veteran/settings.py
+++ veteran/veteran/settings.py
@@ -114,13 +114,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
